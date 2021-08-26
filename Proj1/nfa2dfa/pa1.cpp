1c0cc9bb816a34d8c6e7795af72acfb249f37cca
Shreyank Patel <spatel91@vols.utk.edu>
2021-08-25 23:17:59 -0400

    add pa1 folder

---
+++ Proj1/nfa2dfa/pa1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    string temp;
+    int numState;
+    getline(cin, temp);
+    if(temp == "Initial State:"){
+        cout<<"true";
+    }
+    return 0;
+
+}
